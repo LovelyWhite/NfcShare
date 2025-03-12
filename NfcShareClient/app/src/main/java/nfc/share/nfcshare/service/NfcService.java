1f8dde87e0264ea4567d458b5f353932902c2fd9
suxi <lovelyywhite@gmail.com>
2025-03-12 20:10:54 +0800

    fix: nfc card remove

--- NfcShareClient/app/src/main/java/nfc/share/nfcshare/service/NfcService.java
+++ NfcShareClient/app/src/main/java/nfc/share/nfcshare/service/NfcService.java
@@ -2,14 +2,13 @@ package nfc.share.nfcshare.service;
 
 import static nfc.share.nfcshare.model.MqttChannel.LOG_CHANNEL;
 
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
-import android.os.Bundle;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.hutool.core.util.HexUtil;
 import nfc.share.nfcshare.MainActivity;
@@ -18,6 +17,8 @@ import nfc.share.nfcshare.Utils;
 public class NfcService implements NfcAdapter.ReaderCallback {
     private IsoDep isoDep;
     NfcAdapter nfcAdapter;
+    private String currentCardId = "";
+    private Timer cardCheckTimer;
 
     public NfcService(MainActivity context) {
         nfcAdapter = NfcAdapter.getDefaultAdapter(context);
@@ -25,24 +26,35 @@ public class NfcService implements NfcAdapter.ReaderCallback {
             Utils.addLogs("Not Support NFC");
             return;
         }
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_IMMUTABLE);
-        nfcAdapter.enableForegroundDispatch(context, pendingIntent, null, null);
         nfcAdapter.enableReaderMode(context, this, 1, null);
     }
+
 
     @Override
     public void onTagDiscovered(Tag discoveredTag) {
         if (discoveredTag != null) {
             Utils.blockingQueue.clear();
             try {
-                String cardId = connectCard(discoveredTag);
-                Utils.mqttService.pushMessageToMqtt(LOG_CHANNEL, "Card connected: " + cardId);
+                currentCardId = connectCard(discoveredTag);
+                if (cardCheckTimer != null) {
+                    cardCheckTimer.cancel();
+                }
+                cardCheckTimer = new Timer();
+                cardCheckTimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            sendData(Utils.QUERY_CARD);
+                        } catch (IOException e) {
+                            onTagRemoved();
+                        }
+                    }
+                }, 3000, 3000);
+
+                Utils.mqttService.pushMessageToMqtt(LOG_CHANNEL, "Card connected: " + currentCardId);
             } catch (IOException e) {
                 Utils.addLogs("Connect card failed");
             }
-        } else {
-            // Tag was removed
-            handleTagRemoved();
         }
     }
 
@@ -55,7 +67,7 @@ public class NfcService implements NfcAdapter.ReaderCallback {
         }
         isoDep.connect();
         isoDep.setTimeout(120000);
-        String cardId = HexUtil.encodeHexStr(tag.getId());
+        final String cardId = HexUtil.encodeHexStr(tag.getId());
         String result = sendData(Utils.QUERY_CARD);
         if (result.length() < 4) {
             Utils.addLogs("Card not support");
@@ -64,7 +76,13 @@ public class NfcService implements NfcAdapter.ReaderCallback {
         return cardId;
     }
 
-    private void handleTagRemoved() {
+
+    public String sendData(String command) throws IOException {
+        byte[] decodeHex = HexUtil.decodeHex(command);
+        return HexUtil.encodeHexStr(this.isoDep != null ? isoDep.transceive(decodeHex) : null);
+    }
+
+    public void onTagRemoved() {
         Utils.mqttService.pushMessageToMqtt(LOG_CHANNEL, "Card removed");
         Utils.addLogs("Card removed");
         if (isoDep != null && isoDep.isConnected()) {
@@ -75,10 +93,7 @@ public class NfcService implements NfcAdapter.ReaderCallback {
             }
             isoDep = null;
         }
-    }
-
-    public String sendData(String command) throws IOException {
-        byte[] decodeHex = HexUtil.decodeHex(command);
-        return HexUtil.encodeHexStr(this.isoDep != null ? isoDep.transceive(decodeHex) : null);
+        cardCheckTimer.cancel();
+        cardCheckTimer = null;
     }
 }
